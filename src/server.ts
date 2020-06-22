539deab2c07ea3d3e8daa284d8afa662a272e45e
lucascprazeres <olucascostadp@gmail.com>
2020-06-22 12:28:09 -0300

    feat: cadastro de agendamentos 📅

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Back-end is running on http://localhost:3333/');

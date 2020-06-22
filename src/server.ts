6472efb71b1455d41adf3d67b6f4ce9553b8c0a0
lucascprazeres <olucascostadp@gmail.com>
2020-06-22 10:03:28 -0300

    Setup do projeto 🚀

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use('/', (request, response) => {
+  return response.json({ message: 'Hello goStack 12!' })
+});
+
+app.listen(3333, () => {
+  console.log('🚀 Back-end is running on http://localhost:3333/');
+})

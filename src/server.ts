3f082983a62b297dac78a90cc1a3f25ca5a1c531
lucascprazeres <olucascostadp@gmail.com>
2020-06-22 10:40:00 -0300

    feat: configura o editorconfig, eslint e o prettier 🛠️

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.use('/', (request, response) => {
-  return response.json({ message: 'Hello goStack 12!' })
-});
+app.use('/', (request, response) =>
+  response.json({ message: 'Hello goStack 12!' }),
+);
 
 app.listen(3333, () => {
   console.log('🚀 Back-end is running on http://localhost:3333/');
-})
+});

4055a1a14c6315ca050921116bf2790d2702d6ab
lucascprazeres <olucascostadp@gmail.com>
2020-06-22 11:49:57 -0300

    minor: teste do debbuger do vscode 🐞

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,18 @@ import express from 'express';
 
 const app = express();
 
-app.use('/', (request, response) =>
-  response.json({ message: 'Hello goStack 12!' }),
-);
+app.use(express.json());
+
+app.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+});
 
 app.listen(3333, () => {
   console.log('🚀 Back-end is running on http://localhost:3333/');

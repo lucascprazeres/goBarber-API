a9ddcfcdf77254dfd8d78210c380a09c983f7496
lucascprazeres <olucascostadp@gmail.com>
2020-06-26 09:29:10 -0300

    feat: adiciona atualização de avatar do usuário 🖼️

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,9 +2,13 @@ import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
 export default {
+  directory: tmpFolder,
+
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    destination: tmpFolder,
     filename(request, file, callback) {
       const hash = crypto.randomBytes(10).toString('hex');
 

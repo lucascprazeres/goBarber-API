6e9c258c0653aabbd00d8138151a76f60393cb0c
lucascprazeres <olucascostadp@gmail.com>
2020-07-13 14:21:47 -0300

    ref: altera a estrutura de pastas do projeto para aplicar os princípios de DDD 👷

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,12 +3,12 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import '@shared/infra/typeorm';
+
 import routes from './routes';
-
-import uploadConfig from './config/upload';
-import './database';
-
-import AppError from './errors/AppError';
 
 const app = express();
 

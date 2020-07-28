d93a1a07c3309f75d5e18c970219d5826ca368b6
lucascprazeres <olucascostadp@gmail.com>
2020-07-28 17:58:37 -0300

    fix: conserta erro de nome da interface ITokenPayload

--- src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
+++ src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
@@ -27,7 +27,7 @@ export default function ensureAuthenticated(
   try {
     const decoded = verify(token, authConfig.jwt.secret);
 
-    const { sub } = decoded as TokenPayload;
+    const { sub } = decoded as ITokenPayload;
 
     request.user = {
       id: sub,

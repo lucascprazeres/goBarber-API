64931fcdb786d5a8f555824b97b2a951943e6bcf
lucascprazeres <olucascostadp@gmail.com>
2020-07-31 18:27:06 -0300

    minor: adiciona cache na listagem de providers

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import IUsersRepository from '@modules/users/repositories/iUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -13,12 +14,25 @@ class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ userId }: IRequest): Promise<User[]> {
-    const users = this.usersRepository.findAllProviders({
-      excludedUserId: userId,
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${userId}`,
+    );
+
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        excludedUserId: userId,
+      });
+
+      console.log('A query no banco foi feita');
+
+      await this.cacheProvider.save(`providers-list:${userId}`, users);
+    }
 
     return users;
   }

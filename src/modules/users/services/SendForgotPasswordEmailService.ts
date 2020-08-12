83d64cc4287cae7428cba9fe6d986a6f4eedecf5
lucascprazeres <olucascostadp@gmail.com>
2020-08-12 01:27:09 -0300

    minor: altera a rota no link do email de reset de senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -50,7 +50,7 @@ class SendForgotPasswordEmailService {
         file: forgotPasswordTemplate,
         variables: {
           name: foundUser.name,
-          link: `${process.env.APP_WEB_URL}/reset?token=${token}`,
+          link: `${process.env.APP_WEB_URL}/reset-password?token=${token}`,
         },
       },
     });
